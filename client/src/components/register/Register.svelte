d4a5f066b96567471c500dd9e6567c0914ad4ca8
Kevin Strandberg <kevin@npstrandberg.comm>
2025-05-23 17:04:17 +0200

    Created Register component

---
+++ client/src/components/register/Register.svelte
@@ -0,0 +1,25 @@
+<script>
+    let email = '';
+    let password = '';
+    let confirm_password = '';
+
+    function handleSubmit(){
+        console.log('Form submitted!');
+        console.log('Email:', email);
+        console.log('Password:', password);
+        console.log('Confirm Password:', confirm_password);
+    }
+</script>
+
+<form on:submit={handleSubmit}>
+    <label for="email-input">Email</label>
+    <input type="text" id="email-input" placeholder="Enter email" bind:value={email}>
+
+    <label for="password-input">Password</label>
+    <input type="password" id="password-input" placeholder="Enter password" bind:value={password}>
+
+    <label for="confirm-password-input">Confirm Password</label>
+    <input type="password" id="confirm-password-input" placeholder="Repeat password" bind:value={confirm_password}>
+    <button type="submit">Submit</button>
+</form>
+
