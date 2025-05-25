0dae77a5728c51911bdbb333cc4db9054b6e232c
Kevin Strandberg <kevin@npstrandberg.comm>
2025-05-25 16:02:02 +0200

    Made routing more clear for orderRouter and set up all endpoints.

--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -2,21 +2,50 @@ import { Router } from 'express';
 const router = Router();
 import db from '../database/connection.js';
 
-router.post('/place', async (req, res) => {
+router.get('/', (req, res) => {
+    res.send({ message: 'Hiii!! ٩(＾◡＾)۶' });
+});
+
+router.get('/:id', (req, res) => {
+    res.send({ message: 'Hiii!! ٩(＾◡＾)۶' });
+});
+
+router.post('/', async (req, res) => {
     try {
-        const order = req.body.data.order;
+        const {
+            userId,
+            cryptocurrencyId,
+            orderType,
+            quantityTotal,
+            quantityRemaning,
+            price,
+            status,
+        } = req.body.data.order;
         const placeOrderQuery = {
-            text: 'INSERT INTO orders (user_id, cryptocurrency_id, order_type, quantity_total, quantity_remaining, price, status) VALUES $1, $2, $3, $4, $5, $6, $7',
-            values: [],
+            text: 'INSERT INTO orders (user_id, cryptocurrency_id, order_type, quantity_total, quantity_remaining, price, status) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+            values: [
+                userId,
+                cryptocurrencyId,
+                orderType,
+                quantityTotal,
+                quantityRemaning,
+                price,
+                status,
+            ],
         };
 
         const response = (await db.query(placeOrderQuery)).rows;
-
-
-
     } catch (error) {
         console.log('Database connection error', error);
     }
+});
+
+router.put('/:id', (req, res) => {
+    res.send({ message: 'Hiii!! ٩(＾◡＾)۶' });
+});
+
+router.delete('/:id', (req, res) => {
+    res.send({ message: 'Hiii!! ٩(＾◡＾)۶' });
 });
 
 export default router;
